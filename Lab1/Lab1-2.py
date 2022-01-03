c679098f3c99621d24d0044e7774b31bed94f5e8
MarkKh <khomsan.cho@gmail.com>
2022-01-03 23:01:30 +0700

    Lab 1

---
+++ Lab1/Lab1-2.py
@@ -0,0 +1,5 @@
+name = 'Khomsan Chooon'
+print('Hello Python')
+print('My name is',name)
+print('I love python programming')
+print('ฉันรักเมืองไทย  ฉันรักภาษาไพทอน')
---
+++ Lab1/Lab1-4.py
@@ -0,0 +1,4 @@
+x = 35
+y = 13
+print('x is =',x)
+print('y is =',y)
