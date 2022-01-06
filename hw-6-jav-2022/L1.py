b07389ef8025f14aa2cb019ab48e338ae7362dd5
MarkKh <khomsan.cho@gmail.com>
2022-01-06 22:25:12 +0700

    edit

--- hw-6-jav-2022/L1.py
+++ hw-6-jav-2022/L1.py
@@ -4,8 +4,5 @@ x2 = int(x[1])
 x3 = int(x[2])
 x4 = int(x[3])
 
-maxInput = max(x1, x2, x3, x4)
-minInput = min(x1, x2, x3, x4)
-
-value = x1 + x2 + x3 + x4 - maxInput - minInput
+value = x1 + x2 + x3 + x4 - max(x1, x2, x3, x4) - min(x1, x2, x3, x4)
 print(value)
