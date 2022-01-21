289a141ebdcec09d4e110378ec4302bd1f99a9f8
MarkKh <khomsan.cho@gmail.com>
2022-01-21 13:18:42 +0700

    comment

--- Lab6/L6-4.py
+++ Lab6/L6-4.py
@@ -3,7 +3,7 @@ while True:
     x = int(input())
     if x != 0:
         if x%3 == 0 or x%5 == 0:
-            sum += x
+            sum += x #sum = sum + x
     else:
         break
 print("Sum is",sum)
